302ae36b9b564521cbbabddb3eced5e00472ee40
Rodriguez-Jp <juanrdrz16@gmail.com>
2021-12-26 23:48:09 -0500

    Add the thanks msg

--- app.js
+++ app.js
@@ -1,94 +1,84 @@
 const isValidEmail = (email) => {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  };
-
-
-const isValidPhone = (phone) => {
-    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-    return re.test(String(phone).toLowerCase());
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 };
 
-const inputName = document.querySelector('.input-name');
-const form = document.querySelector('form');
-const validationTxt =  document.querySelectorAll('.validation');
-const inputEmail = document.querySelector('.input-email');
-const inputLastName = document.querySelector('.input-last-name');
-const inputPhone = document.querySelector('.input-phone');
+const isValidPhone = (phone) => {
+  const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(String(phone).toLowerCase());
+};
 
+const inputName = document.querySelector(".input-name");
+const form = document.querySelector("form");
+const validationTxt = document.querySelectorAll(".validation");
+const inputEmail = document.querySelector(".input-email");
+const inputLastName = document.querySelector(".input-last-name");
+const inputPhone = document.querySelector(".input-phone");
+const thanksMsg = document.querySelector(".thanks-msg-container");
 
 let isValid = false;
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    validateInputs();
-    if (isValid == true){
-        form.remove();
-    }
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+  if (isValid == true) {
+    form.remove();
+    thanksMsg.classList.add("show");
+  }
+});
 
+const validateInputs = () => {
+  validationTxt[0].classList.add("hidden");
+  inputName.classList.remove("invalid");
 
+  validationTxt[1].classList.add("hidden");
+  inputLastName.classList.remove("invalid");
 
-const validateInputs = () =>{
+  validationTxt[2].classList.add("hidden");
+  inputEmail.classList.remove("invalid");
 
+  validationTxt[3].classList.add("hidden");
+  inputPhone.classList.remove("invalid");
 
-    
-    validationTxt[0].classList.add('hidden');
-    inputName.classList.remove('invalid');
+  isValid = true;
 
-    validationTxt[1].classList.add('hidden');
-    inputLastName.classList.remove('invalid');
+  if (!inputName.value) {
+    validationTxt[0].classList.remove("hidden");
+    inputName.classList.add("invalid");
+    isValid = false;
+  }
+  if (!inputLastName.value) {
+    validationTxt[1].classList.remove("hidden");
+    inputLastName.classList.add("invalid");
+    isValid = false;
+  }
 
-    validationTxt[2].classList.add('hidden');
-    inputEmail.classList.remove('invalid');
+  if (!isValidEmail(inputEmail.value)) {
+    validationTxt[2].classList.remove("hidden");
+    inputEmail.classList.add("invalid");
+    isValid = false;
+  }
 
-    validationTxt[3].classList.add('hidden');
-    inputPhone.classList.remove('invalid');
+  if (!isValidPhone(inputPhone.value)) {
+    validationTxt[3].classList.remove("hidden");
+    inputPhone.classList.add("invalid");
+    isValid = false;
+  }
+};
 
+inputName.addEventListener("input", () => {
+  validateInputs();
+});
 
-    isValid = true;
+inputEmail.addEventListener("input", () => {
+  validateInputs();
+});
 
+inputLastName.addEventListener("input", () => {
+  validateInputs();
+});
 
-    if(!inputName.value){
-        validationTxt[0].classList.remove('hidden');
-        inputName.classList.add('invalid');
-        isValid = false;
-    }
-    if(!inputLastName.value){
-        validationTxt[1].classList.remove('hidden');
-        inputLastName.classList.add('invalid');
-        isValid = false;
-    }
-
-    if(!isValidEmail(inputEmail.value)){
-        validationTxt[2].classList.remove('hidden');
-        inputEmail.classList.add('invalid');
-        isValid = false;
-    }
-
-    if(!isValidPhone(inputPhone.value)){
-        validationTxt[3].classList.remove('hidden');
-        inputPhone.classList.add('invalid');
-        isValid = false;
-    }
-
-
-
-}
-
-inputName.addEventListener('input', ()=>{
-    validateInputs();
-})
-
-inputEmail.addEventListener('input', ()=>{
-    validateInputs();
-})
-
-inputLastName.addEventListener('input', ()=>{
-    validateInputs();
-})
-
-inputPhone.addEventListener('input', ()=>{
-    validateInputs();
-})
-
+inputPhone.addEventListener("input", () => {
+  validateInputs();
+});
